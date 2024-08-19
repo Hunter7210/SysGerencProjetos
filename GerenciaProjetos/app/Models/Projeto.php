e9d98236207b17471ffb19d6060f0e5f40b90e3b
Hunter7210 <heitorrmsilva@gmail.com>
2024-08-19 16:41:06 -0300

    Up  models and migrations

--- GerenciaProjetos/app/Models/Projeto.php
+++ GerenciaProjetos/app/Models/Projeto.php
@@ -4,8 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Projeto extends Model
 {
     use HasFactory;
+
+    protected $table = 'projetos'; // Nome da tabela
+
+    protected $primaryKey = 'idProjetoPk'; // Chave primária
+
+    public $incrementing = true; // Chave primária é auto-incrementável
+
+    protected $fillable = [
+        'nomeProjeto',
+        'descricaoProjeto',
+        'terminoProjeto',
+        'responsaveisProjeto',
+        'criadorProjetoFk',
+        'equipeProjetoFk',
+    ];
+
+    // Relação com o modelo Usuario
+    public function criador()
+    {
+        return $this->belongsTo(User::class, 'criadorProjetoFk', 'id');
+    }
+
+    // Relação com o modelo Equipe
+    public function equipe()
+    {
+        return $this->belongsTo(Equipe::class, 'equipeProjetoFk', 'id');
+    }
 }
--- GerenciaProjetos/database/migrations/2024_08_19_192402_create_projetos_table.php
+++ GerenciaProjetos/database/migrations/2024_08_19_192402_create_projetos_table.php
@@ -4,24 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateProjetosTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('projetos', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id(); // Primary key
+            $table->string('nomeProjeto'); // VARCHAR(255)
+            $table->string('descricaoProjeto'); // VARCHAR(255)
+            $table->dateTime('terminoProjeto')->nullable(); // DATETIME, nullable se não for obrigatório
+            $table->string('responsaveisProjeto'); // VARCHAR(255)
+            $table->unsignedBigInteger('criadorProjetoFk'); // FOREIGN KEY Usuario
+            $table->unsignedBigInteger('equipeProjetoFk'); // FOREIGN KEY equipe
+            $table->timestamps(); // horaCriado (created_at, updated_at)
+
+            // Definindo as chaves estrangeiras
+            $table->foreign('criadorProjetoFk')->references('id')->on('usuarios')->onDelete('cascade');
+            $table->foreign('equipeProjetoFk')->references('id')->on('equipes')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('projetos');
     }
-};
+}
