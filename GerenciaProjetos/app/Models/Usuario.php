b6282b7fc528eb5f9a65168ccc29b3fbfe31506d
Hunter7210 <heitorrmsilva@gmail.com>
2024-08-19 16:53:51 -0300

    Up  models and migrations

--- GerenciaProjetos/app/Models/Usuario.php
+++ GerenciaProjetos/app/Models/Usuario.php
@@ -14,6 +14,7 @@ class Usuario extends Model
         'nomeUsuario',
         'emailUsuario',
         'cargoUsuario',
+        'nomeGerenteUsuario',
         'nomeEmpresaUsuario',
         'cpfUsuario',
         'passwordUsuario'
